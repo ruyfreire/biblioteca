a56b92687d295ff7ab1bc934b34420b37cbb6002
Ruy Freire <ruy.slv@gmail.com>
2019-12-01 02:04:25 -0300

    criando componente para aside

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import Aside from "./components/aside/Aside";
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = { asideOpen: false };
-  }
-  open = () => this.setState({ asideOpen: !this.state.asideOpen });
-
   render() {
     return (
       <div className="app-container">
-        <aside className={this.state.asideOpen ? "open" : ""}>
-          <div className="aside-container">
-            <ul>
-              <li className="menu" onClick={this.open}>
-                <a href=" #">
-                  <i className="fas fa-bars"></i>Menu
-                </a>
-              </li>
-              <li className="home">
-                <Link to={"/"}>
-                  <i className="fas fa-home"></i>Home
-                </Link>
-              </li>
-              <li className="authors">
-                <Link to={"/authors"}>
-                  <i className="fas fa-user"></i>Autores
-                </Link>
-              </li>
-              <li className="books">
-                <Link to={"/books"}>
-                  <i className="fas fa-book"></i>Livros
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </aside>
-
+        <Aside />
         <main>{this.props.children}</main>
       </div>
     );
