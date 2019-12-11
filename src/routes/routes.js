873aff0a45dc16a3064adce93afdff4cd5d16469
Ruy Freire <ruy.slv@gmail.com>
2019-12-11 00:08:08 -0300

    remove página de manutenção

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "../App";
 import Home from "../templates/home/Home";
 import AuthorBox from "../templates/author/Authors";
-// import BookBox from "../templates/book/Books";
-import PageWorking from "../templates/anyPages/PageWorking";
+import BookBox from "../templates/book/Books";
+// import PageWorking from "../templates/anyPages/PageWorking";
 import NotFound from "../templates/anyPages/NotFound";
 
 const rotas = () => {
@@ -16,8 +16,8 @@ const rotas = () => {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/authors" component={AuthorBox} />
-            {/* <Route path="/books" component={BookBox} /> */}
-            <Route path="/books" component={PageWorking} />
+            <Route path="/books" component={BookBox} />
+            {/* <Route path="/books" component={PageWorking} /> */}
             <Route path="*" component={NotFound} />
           </Switch>
         </App>
